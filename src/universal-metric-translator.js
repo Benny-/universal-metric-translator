eb33da72b4f8c81b6180ab0b29c9a0830d071937
Benny <Benny@gmx.it>
2015-01-14 21:59:12 +0100

    removed currency for realz this time

--- src/universal-metric-translator.js
+++ src/universal-metric-translator.js
@@ -168,13 +168,6 @@ var tranformationTable = [
         conversation: 50.80234544,
     },
     //  A 'ton' might belong here, but there exist a metric ton and a imperial ton.
-    
-    // Currency
-//    {
-//        from: 'dollars?',
-//        to: '€',
-//        conversation: 0.75, // NOTE: This conversation depends on exchange rates. It will vary over time.
-//    },
 ];
 
 tranformationTable.forEach(function (transformationRule) {
