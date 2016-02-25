3fa64f1343e41664d03b9250cd25dbe7e4157886
Benny <Benny@gmx.it>
2016-02-25 16:03:39 +0100

    Description and namespace change

--- src/browser/userscript.js
+++ src/browser/userscript.js
@@ -1,12 +1,10 @@
 // ==UserScript==
 // @name        Universal metric translator
-// @namespace   https://㑤.com/universal-metric-translator.user.js
-// @updateURL   https://㑤.com/universal-metric-translator.meta.js
-// @downloadURL https://㑤.com/universal-metric-translator.user.js
-// @description Converts imperial units to metric units
+// @namespace   https://bennyjacobs.nl/userscripts/Universal-metric-translator
+// @description Automatically converts imperial units to metric units
 // @include     about:addons
 // @include     http*
 // @include     https*
-// @version     2.0.1
+// @version     2.0.2
 // @grant       none
 // ==/UserScript==
