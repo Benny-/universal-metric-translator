84611516e6bdc53ec4bb552a3467f4f5607e308e
Benny Jacobs <Benny@gmx.it>
2017-07-06 00:07:23 +0200

    Added BSD3 license

--- src/browser/userscript.js
+++ src/browser/userscript.js
@@ -2,9 +2,10 @@
 // @name        Universal metric translator
 // @namespace   https://bennyjacobs.nl/userscripts/Universal-metric-translator
 // @description Automatically converts imperial units to metric units
+// @license     BSD-3-Clause
 // @include     about:addons
 // @include     http*
 // @include     https*
-// @version     2.1.1
+// @version     2.1.2
 // @grant       none
 // ==/UserScript==
